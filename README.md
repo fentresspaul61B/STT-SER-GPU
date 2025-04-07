e3ff187b79975581eb57c9d0666234ff32dec455
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2025-04-07 13:01:04 -0700

    updating readme.

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# STT-SER-GPU
-An API performs speech to text and speech emotion recognition with multiprocessing to speed up response times. 
+# STT+SER with GPU on Google Cloud Run
 
-.
+## Overview
+This project shows an example API, which performs Speech Emotion Recognition (SER)
+and Speech To Text (STT) in parallel with GPUs. This is useful if you want to 
+translate speech to text and analyze the emotional tone of the speaker at the 
+same time. 
+
+This API is simple in the sense, that all it does is just call two other APIs, 
+joins there responses, and returns them as one response. 
+
+This project builds on top of two previous ones, which are:
+- Whisper STT on GPU: https://github.com/fentresspaul61B/Deploy-Whisper-On-GCP
+- SER on GPU: https://github.com/fentresspaul61B/SER-with-GPU
+
+How it works:
+
+1. Takes in a single audio file as an argument to the API.
+2. Generates a GCP tokens from within the GCP Cloud Run Server. 
+3. Use Async Gather method to call both APIs at the same with their respective 
+tokens. 
+4. Join and return the responses. 
+
+## Challenges 
+The biggest challenge that I ran into, was how to call a GCP API from inside
+another GCP API. I tried 3 ways of solving this, but only one worked. 
+
+1. ⚠️ Passing the token from the orchestrator API to the two sub APIs: Failed due 
+to permissions issues. 
+2. ⚠️ Generating the token in GCP env by downloading and invoking the CLI. This 
+failed as well, as I struggled to download and utilize the CLI tool from within
+the GCP env. 
+3. ✅ Use the Google Auth Python Package: Worked! There is a specific function 
+created for generating tokens while inside a GCP cloud environment. The docs 
+are here: https://googleapis.dev/python/google-auth/latest/user-guide.html#identity-tokens 
+
+    ```python
+    def generate_token_in_gcp_env(url: str):
+        """This function generates a token while inside GCP cloud run."""
+        auth_req = google.auth.transport.requests.Request()
+        id_token = google.oauth2.id_token.fetch_id_token(auth_req, url)
+        return id_token
+
+
+    ```
+
+I am glad that I found this specific token method, as I always struggled with this problem of figuring out how to orchestrate GCP APIs and dealing with 
+permissions issues in the different environments, but this solves that 
+issue. Using this method also avoids needing to upload any auth secrets required for service accounts, which can be headache and bad for security purposes. 
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
-from fastapi import FastAPI, Request, HTTPException, File, UploadFile
-import httpx
+from fastapi import FastAPI, Request, File, UploadFile
 from configs import SER_URL, STT_URL
 import subprocess
 import tempfile
@@ -21,25 +20,25 @@ def save_upload_file_to_temp(upload_file: UploadFile) -> str:
     return temp_file_path
 
 
-def generate_token(url: str):
+def generate_token_in_gcp_env(url: str):
+    """This function generates a token while inside GCP cloud run."""
     auth_req = google.auth.transport.requests.Request()
     id_token = google.oauth2.id_token.fetch_id_token(auth_req, url)
     return id_token
 
 
-# def generate_token():
-#     """Generates GCP identity token for authentication."""
-#     command = ["gcloud", "auth", "print-identity-token"]
-#     token = subprocess.check_output(command).decode("utf-8").strip()
-#     print("GCP identity token generated. ✔️")
-#     return token
+def generate_token():
+    """Generates GCP identity token for authentication. This is used for local
+    testing."""
+    command = ["gcloud", "auth", "print-identity-token"]
+    token = subprocess.check_output(command).decode("utf-8").strip()
+    print("GCP identity token generated. ✔️")
+    return token
 
 
-async def call_api_with_file(url: str, token: str, file: UploadFile):
+async def call_api_with_audio_file(url: str, token: str, file: UploadFile):
     temp_filepath = save_upload_file_to_temp(file)
-
     with open(temp_filepath, "rb") as audio_file:
-        # Create a dictionary of files to be uploaded
         files = {
             "file": (temp_filepath.split("/")[-1], audio_file, "audio/mpeg")
         }
@@ -60,44 +59,18 @@ async def call_api_with_file(url: str, token: str, file: UploadFile):
 
 @app.post("/translate/")
 async def translate(request: Request, file: UploadFile = File(...)):
-    # auth_header = request.headers.get("Authorization")
-
-    # token = auth_header[len("Bearer "):]
-    # print(token)
+    """API endpoint, which performs SER and STT at the same time."""
     start_time = time.time()
+    ser_token = generate_token_in_gcp_env(SER_URL)
+    stt_token = generate_token_in_gcp_env(STT_URL)
     ser_response, stt_response = await asyncio.gather(
-        call_api_with_file(SER_URL, generate_token(SER_URL), file),
-        call_api_with_file(STT_URL, generate_token(STT_URL), file)
+        call_api_with_audio_file(SER_URL, ser_token, file),
+        call_api_with_audio_file(STT_URL, stt_token, file)
     )
     end_time = time.time()
     processing_time = end_time - start_time
     return {
-        "processing_time_seconds": processing_time,
+        "total_processing_time": processing_time,
         "ser": ser_response,
         "stt": stt_response
     }
-
-
-@app.post("/simple_translate/")
-async def simple_translate(request: Request, file: UploadFile = File(...)):
-    temp_filepath = save_upload_file_to_temp(file)
-
-    with open(temp_filepath, "rb") as audio_file:
-        # Create a dictionary of files to be uploaded
-        files = {
-            "file": (temp_filepath.split("/")[-1], audio_file, "audio/mpeg")
-        }
-        headers = {
-            "Authorization": f"Bearer {generate_token()}"
-        }
-        print(type(audio_file))
-
-        # files = {'file': open(temp_filepath, 'rb')}
-    
-        response = requests.post(STT_URL, headers=headers, files=files)
-        print(f"Response Status Code: {response.status_code}")
-        print(f"Response Headers: {response.headers}")
-        print("made the request")
-        return response.json()
-    return {}
-
