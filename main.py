1fb0aef8b4ecaf2dc035f5124a787113c2697c39
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2025-04-07 02:51:31 -0700

    almost got it working locally. Made successful internal API request to SER, but not both yet. Issues was likely due to audio file reading and bytes issues.

--- main.py
+++ main.py
@@ -3,19 +3,38 @@ import time
 from fastapi import FastAPI, Request, HTTPException, File, UploadFile
 import httpx
 from configs import SER_URL, STT_URL
+import subprocess
+import tempfile
+import shutil
+import requests
 
 app = FastAPI()
+
+
+def save_upload_file_to_temp(upload_file: UploadFile) -> str:
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        upload_file.file.seek(0)
+        shutil.copyfileobj(upload_file.file, temp_file)
+        temp_file_path = temp_file.name
+    return temp_file_path
+
+
+def generate_token():
+    """Generates GCP identity token for authentication."""
+    command = ["gcloud", "auth", "print-identity-token"]
+    token = subprocess.check_output(command).decode("utf-8").strip()
+    print("GCP identity token generated. ✔️")
+    return token
 
 
 async def call_api_with_file(url: str, token: str, file: UploadFile):
     headers = {
         "Authorization": f"Bearer {token}"
     }
-    # Important: reset file pointer to beginning for each API call (?)
-    await file.seek(0)
     files = {
-        "file": (file.filename, await file.read(), file.content_type)
+        "file": (file.filename, await file.read(), 'audio/mpeg')
     }
+
     async with httpx.AsyncClient(timeout=60) as client:
         response = await client.post(url, headers=headers, files=files)
     if response.status_code != 200:
@@ -28,14 +47,14 @@ async def call_api_with_file(url: str, token: str, file: UploadFile):
 
 @app.post("/translate/")
 async def translate(request: Request, file: UploadFile = File(...)):
-    auth_header = request.headers.get("Authorization")
+    # auth_header = request.headers.get("Authorization")
     
-    token = auth_header[len("Bearer "):]
-    print(token)
+    # token = auth_header[len("Bearer "):]
+    # print(token)
     start_time = time.time()
     ser_response, stt_response = await asyncio.gather(
-        call_api_with_file(SER_URL, token, file),
-        call_api_with_file(STT_URL, token, file)
+        call_api_with_file(SER_URL, generate_token(), file),
+        call_api_with_file(STT_URL, generate_token(), file)
     )
     end_time = time.time()
     processing_time = end_time - start_time
@@ -44,3 +63,28 @@ async def translate(request: Request, file: UploadFile = File(...)):
         "ser": ser_response,
         "stt": stt_response
     }
+
+
+@app.post("/simple_translate/")
+async def simple_translate(request: Request, file: UploadFile = File(...)):
+    temp_filepath = save_upload_file_to_temp(file)
+
+    with open(temp_filepath, "rb") as audio_file:
+        # Create a dictionary of files to be uploaded
+        files = {
+            "file": (temp_filepath.split("/")[-1], audio_file, "audio/mpeg")
+        }
+        headers = {
+            "Authorization": f"Bearer {generate_token()}"
+        }
+        print(type(audio_file))
+
+        # files = {'file': open(temp_filepath, 'rb')}
+    
+        response = requests.post(SER_URL, headers=headers, files=files)
+        print(f"Response Status Code: {response.status_code}")
+        print(f"Response Headers: {response.headers}")
+        print("made the request")
+        return response.json()
+    return {}
+
---
+++ test.py
@@ -0,0 +1,10 @@
+from main import generate_token
+
+
+def main():
+    print(generate_token())
+    pass
+
+
+if __name__ == "__main__":
+    main()
